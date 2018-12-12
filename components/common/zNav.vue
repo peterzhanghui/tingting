58e51134ebbd299aa52b5b6b62bf072e732a49b3
zhanghui <zhanghui@dong10.com>
2018-12-12 17:32:54 +0800

    add home

--- components/common/zNav.vue
+++ components/common/zNav.vue
@@ -115,7 +115,6 @@
       transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
     }
     .slide-fade-enter, .slide-fade-leave-to
-      /* .slide-fade-leave-active for below version 2.1.8 */
     {
       transform: translateX(10px);
       opacity: 0;
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <section class="container">
     <div class="banner_box">
+
       <div class="content"><p>欲穷千里目</p><p>更上一层楼</p></div>
-      <div class="banner"></div>
+      <transition name="slide-fade">
+        <div class="banner"></div>
+      </transition>
     </div>
     <div>
       <!--<img src='https://open.saintic.com/api/bingPic/' />-->
@@ -24,31 +27,77 @@
 </script>
 
 <style type="text/css" lang="scss">
-  .container {
-    width: 100%;
-    min-height: 90vh;
-    .banner_box{
-      display: flex;
-      flex-grow: 1;
-      align-items: center;
-      .banner {
-        flex-grow: 1;
-        width: 65%;
-        height: 70vh;
-        background: url("../static/banner.jpg") no-repeat;
-        background-size: cover;
-      }
-      .content{
-        transform: translateX(50px);
-        margin-left: -50px;
-        width: 35%;
-        height: 200px;
-        background: #fff;
-        color: black;
-        box-shadow: 0.5rem 0 4rem -1rem rgba(0,0,0,.35);
-        z-index: 10;
+  @media screen and (max-width: 480px){
+    .container {
+      width: 100%;
+      min-height: 90vh;
+      .banner_box{
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: center;
+        .banner {
+          flex: 1;
+          order: 1;
+          width: 100%;
+          height: 70vh;
+          background: url("../static/banner.jpg") no-repeat;
+          background-size: cover;
+        }
+        .content{
+          order: 2;
+          transform: translateY(-80px);
+          /*<!--margin-top: -100px;-->*/
+          width: 60%;
+          height: 200px;
+          background: #fff;
+          color: black;
+          box-shadow: 0.5rem 0 4rem -1rem rgba(0,0,0,.35);
+          z-index: 10;
+          text-align: center;
+          line-height: 40px;
+          font-size: 24px;
+          padding-top: 40px;
+          letter-spacing: 10px;
+          font-weight: bold;
+        }
       }
     }
-
   }
+  @media screen and (min-width: 480px){
+    .container {
+      width: 100%;
+      min-height: 90vh;
+      .banner_box{
+        padding: 100px 20px;
+        display: flex;
+        align-items: center;
+        .banner {
+          flex: 1;
+          width: 76%;
+          height: 70vh;
+          background: url("../static/banner.jpg") no-repeat;
+          background-size: cover;
+        }
+        .content{
+          transform: translateX(100px);
+          margin-left: -100px;
+          width: 20%;
+          height: 200px;
+          background: #fff;
+          color: black;
+          box-shadow: 0.5rem 0 4rem -1rem rgba(0,0,0,.35);
+          z-index: 10;
+          text-align: center;
+          line-height: 40px;
+          font-size: 24px;
+          padding-top: 40px;
+          letter-spacing: 10px;
+          font-weight: bold;
+        }
+      }
+    }
+  }
+
 </style>
