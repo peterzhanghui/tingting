98d727a2fb1bd8c1c3f425fdb9682b99bf3a005f
zhanghui <zhanghui@dong10.com>
2019-05-09 18:58:02 +0800

    添加图片样式

--- pages/life/index.vue
+++ pages/life/index.vue
@@ -1,38 +1,72 @@
 <template>
   <section class="container ">
-    <div class="life"></div>
-    <div class="life life1" style=""></div>
-    <div class="life life2"></div>
+    <!--<div class="life"></div>-->
+    <!--<div class="life life1" style=""></div>-->
+    <!--<div class="life life2"></div>-->
+    <div class="box g-r-center">
+      <span class="content">
+        中文厉害$￥
+      </span>
+
+      <img class="header img " src="~/assets/img/header.png" alt="">
+      <img class="right img" src="~/assets/img/right.png" alt="">
+    </div>
   </section>
 </template>
 
 <script>
 
-export default {
-  data() {
-    return {
+  export default {
+    data() {
+      return {}
     }
   }
-}
 </script>
 
 <style>
-  .container{
+  .box {
+    line-height: 120px;
+    height: 120px;
+    width: 90%;
+    margin: 0 auto;
+    border: 1px solid;
+  }
+
+  .content {
+    flex: auto;
+  }
+
+  .header {
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+    overflow: hidden;
+  }
+
+  .right {
+    width: 26px;
+    height: 26px;
+  }
+
+  .container {
     margin: 0 auto;
   }
-  .life{
+
+  .life {
     height: 200px;
     width: 200px;
     background: #2A7FF3;
     margin: 0 auto;
   }
-  .life1{
+
+  .life1 {
     background: #40BF88;
     width: 300px;
     transition: 300ms;
     transform: translateY(20px);
   }
-  .life2{
+
+  .life2 {
     background: #FD8C37;
   }
 </style>
